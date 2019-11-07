41d89b4fb1f3ebc4d43c3f17510a7170d02a2be1
IvanDruhak <attlantick@gmai.com>
2019-11-07 13:02:18 +0200

    Visual improved

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,15 +1,23 @@
 <template>
   <div class="hello">
+    <b-container class="text-center">
+      <b-card-group>
+        <b-card>
+          <b-card-title  class="text-right" >
+            <b-button :disable="false" href="#" @click="editMode = !editMode" variant="primary">
+              {{editMode ? 'List mode': 'Edit mode'}}
+            </b-button>
+          </b-card-title>
+          <b-card-body>
+            <b-form>
+              <list-item v-for="(item,index) in items.list()" @del="items.del(Number(index))" :disable="!editMode" :key="index" :item="item"/>
+              <list-item @add="add" :key="'add'"/>
+            </b-form>
+          </b-card-body>
+        </b-card>
+      </b-card-group>
 
-    <ul>
-      <list-item v-for="(item,index) in items.list()" @del="items.del(Number(index))" :disable="!editMode" :key="index" :item="item"/>
-      <list-item @add="add" :key="'add'"/>
-    </ul>
-
-    <b-button :disable="false" href="#" @click="editMode = !editMode" variant="primary">
-      {{editMode ? 'List': 'Edit'}}
-    </b-button>
-
+    </b-container>
   </div>
 </template>
 
@@ -31,7 +39,8 @@
     }
 
     add(text: string){
-      this.items.add(text)
+      if (text)
+        this.items.add(text)
     }
   }
 </script>
